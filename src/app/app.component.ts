3e4b027d61a6a627fd5097064aba9be721a7cd85
lullyferreira <ferreira.dev.go@gmail.com>
2023-10-09 21:33:26 -0300

    diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'terceiro-projeto';
+
+  mostrar = true;
+
+  inverter(){
+    this.mostrar = !this.mostrar;
+  }
 }
